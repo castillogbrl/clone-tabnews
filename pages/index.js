86dced5b9e55cf4eeaa8ac1c880e870691485516
Gabriel Castillo <castillogabriel.cc@gmail.com>
2025-05-06 23:47:45 -0400

    modifica a frase principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Já que tu queria uma caça ao tesouro. Aí vai: em cima da geladeira e embaixo do pano. Boa sorte.</h1>
+    return <h1>Já que tu queria uma caça ao tesouro. Aí vai: em cima da geladeira e embaixo do pano. Boa sorte 😍</h1>
 }
 
 export default Home;
